ae010f6153e140785f11c49a2caea005b5e7187d
Fredy_Vanegas <fredy.vanegas@unincca.edu.co>
2018-10-17 16:08:05 -0500

    Configurando middleware

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const session = require('cookie-session');
 const port = process.env.PORT;
 const app = express();
 
-app.listen(port, function(){
+//middleware
+app.use(morgan('dev')); //Información detallada en el terminal
+app.use(bodyParser.urlencoded({
+    extended: true
+})); //Obtener los datos de las peticiones poste en el atributo "body" del request
+app.use(session({
+    secret: 'node'
+})); //Configuración cookie-session (persistencia en sesión)
+
+
+//verificar en qué puerto se está trabajando
+app.listen(port, function () {
     console.log('Escuchando en el puerto ', port);
 })
